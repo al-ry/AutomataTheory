12a55526a44811ed998aa78a235fec3e20dfa38d
al-ry <saska_ryb@mail.ru>
2021-04-27 18:19:24 +0300

    da

--- LLGenerator/LLGenerator/Utils.cpp
+++ LLGenerator/LLGenerator/Utils.cpp
@@ -10,28 +10,85 @@ void AddRule(Grammar& grammar, const std::string& left, const Rules& right)
 }
 
 
-std::vector<std::string> GetRules(const std::string& rules)
+std::vector<Rules> GetRules(const std::string& rules)
 {
 	std::stringstream ss(rules);
-	std::string rule;
-	std::vector<std::string> rulesArr;
-	while (ss >> rule)
+	std::string ruleStr;
+	std::vector<Rules> rulesArr;
+	Rules rule;
+	while (ss >> ruleStr)
 	{
-		if (rule == RULE_SEPARATOR)
+		if (ruleStr == RULE_SEPARATOR)
 		{
+			rulesArr.push_back(rule);
+			rule.right.clear();
 			continue;
 		}
-		rulesArr.push_back(rule);
+		rule.right.push_back(ruleStr);
 	}
+	rulesArr.push_back(rule);
 	return rulesArr;
 }
 
-void FactorizeRule(Rules& rules)
+void Factorize(Grammar& grammar, std::vector<Rules> similiarRules)
 {
-	rules.right;
-	std::vector<std::string> tmp;
-	for (auto const &item:rules.right)
+	for (auto& rule : similiarRules)
 	{
+		std::vector<std::string> similiarPart;
+		auto it = std::find_if(similiarRules.begin(), similiarRules.end(), [&rule, &similiarPart](const Rules& ruleParam) {
+			bool result = false;
+			for (auto& symbl : rule.right)
+			{
+				for (auto& innerSymbl : ruleParam.right)
+				{
+					if (symbl == innerSymbl)
+					{
+						result = true;
+						similiarPart.push_back(symbl);
+					}
+					else
+					{
+						return result;
+					}
+				}
+			}
+		});
+		if (it != similiarRules.end())
+		{
+			return;
+		}
+	}
+}
+void RemoveRecursion(Grammar& grammar)
+{
+
+}
+
+void FactorizeGrammar(Grammar& grammar, const std::vector<std::string>& nonterminals)
+{
+	std::vector<std::string> tmp;
+	for (auto & nonterminal : nonterminals)
+	{
+		std::vector<Rules> similiarRules;
+		for (auto it = grammar.begin(); it != grammar.end(); it++)
+		{
+			
+			it = std::find_if(it, grammar.end(), [&](const Rules& rule) {
+				return nonterminal == rule.left;
+			});
+			if (it == grammar.end())
+			{
+				break;
+			}
+			if (std::find_if(similiarRules.begin(), similiarRules.end(), [&](const Rules &rule) {
+				return rule.right == it->right;
+			}) == similiarRules.end())
+			{
+				similiarRules.push_back(*it);
+			}
+		}
+		Factorize(grammar, similiarRules);
+		//RemoveRecursion()
 		//std::find_if()
 	}
 }
@@ -58,17 +115,22 @@ Grammar CreateGrammar(const std::string grammarStr)
 	while (std::getline(ss, line))
 	{
 		std::stringstream ruleLine(line);
-		std::string separator;
+		std::string tmp;
 		std::string rightPart;
-		Rules rules;
-		ruleLine >> rules.left;
-		AddNonteminal(nonterminals, rules.left);
-		ruleLine >> separator;
+		std::string leftPart;
+		ruleLine >> leftPart;
+		AddNonteminal(nonterminals, leftPart);
+		ruleLine >> tmp; //  reading ->
 		std::getline(ruleLine, rightPart);
-		rules.right = GetRules(rightPart);
-		//FactorizeRule(rules);
-		grammar.push_back(rules);
+		std::vector<Rules> rules;
+		rules = GetRules(rightPart);
+		for (auto &rule: rules)
+		{
+			rule.left = leftPart;
+			grammar.push_back(rule);
+		}
 	}
+	FactorizeGrammar(grammar, nonterminals);
 	return grammar;
 }
 
--- LLGenerator/LLGenerator/main.cpp
+++ LLGenerator/LLGenerator/main.cpp
@@ -6,6 +6,6 @@ int main()
 
 	Grammar gr; 
 
-	CreateGrammar("shit -> a | b | A\nshit2\nshit3\nshit");
+	CreateGrammar("<S> -> <A> <B> <C> _|_ | <A> a\n<A> -> <A> a\n<A> -> e\n<B> -> <B> b\n<B> -> e\n<C> -> <C> c\n<C> -> e");
 	return 0;
 }
