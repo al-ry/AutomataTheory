6a6c75e09d272bb775e2617cacd4bd878a12917a
al-ry <saska_ryb@mail.ru>
2021-06-28 10:24:12 +0300

    deleted extra function

--- LL(1)/Analyzer/Analyzer.cpp
+++ LL(1)/Analyzer/Analyzer.cpp
@@ -29,86 +29,6 @@ void PrintStack(std::stack<size_t> s)
 	s.push(x);
 }
 
-void IterateTable(Table table, std::vector<std::string> inputSequence, size_t ruleIndex, size_t sequenceIndex, std::stack<size_t> stack, std::vector<std::string>& pointerVector)
-{
-	pointerVector.push_back(std::to_string(ruleIndex));
-	auto foundInGuideSet = std::find(table[ruleIndex].guideSet.begin(), table[ruleIndex].guideSet.end(), inputSequence[sequenceIndex]);
-
-	if (foundInGuideSet != table[ruleIndex].guideSet.end())
-	{
-		if (table[ruleIndex].end && sequenceIndex == inputSequence.size() - 1 && stack.empty())
-		{
-			std::cout << "Ok\n";
-		}
-		else if (!table[ruleIndex].isInStack)
-		{
-			if (table[ruleIndex].shift)
-			{
-				if (sequenceIndex != inputSequence.size() - 1)
-				{
-					sequenceIndex++;
-				}
-				else
-				{
-					std::cout << "Error. Sequence is empty\n";
-					return;
-				}
-			}
-
-			if (table[ruleIndex].pointer.has_value())
-			{
-				IterateTable(table, inputSequence, table[ruleIndex].pointer.value(), sequenceIndex, stack, pointerVector);
-			}
-			else if (!stack.empty())
-			{
-				auto row = stack.top();
-				//std::cout << "From stack " + std::to_string(row) + "\n";
-				//stack.pop();
-				//std::cout << "Stack: \n";
-				//PrintStack(stack);
-				//std::cout << "\n";
-				IterateTable(table, inputSequence, row, sequenceIndex, stack, pointerVector);
-			}
-		}
-		else
-		{
-			//std::cout << "To stack " + std::to_string(ruleIndex + 1) + "\n";
-			//std::cout << "Stack: \n";
-			//PrintStack(stack);
-			//std::cout << "\n";
-			stack.push(ruleIndex + 1);
-			IterateTable(table, inputSequence, table[ruleIndex].pointer.value(), sequenceIndex, stack, pointerVector);
-		}
-	}
-	else if (!table[ruleIndex].error)
-	{
-		IterateTable(table, inputSequence, ruleIndex + 1, sequenceIndex, stack, pointerVector);
-	}
-	else
-	{
-		std::cout << "Error " + std::to_string(ruleIndex) + "\n";
-	}
-}
-
-void AnalyzeTable(Table table, std::vector<std::string> inputSequence)
-{
-	std::cout << "Input sequence: ";
-	for (auto index : inputSequence)
-	{
-		std::cout << index + " ";
-	}
-
-	std::cout << "\n";
-	std::stack<size_t> stack;
-	std::vector<std::string> pointerVector;
-	IterateTable(table, inputSequence, 0, 0, stack, pointerVector);
-	std::cout << "Pointer sequence: ";
-	for (auto index : pointerVector)
-	{
-		std::cout << index + " ";
-	}
-}
-
 
 void IterateTable(Table table, Lexer& lexer, size_t ruleIndex, Token currentToken, std::stack<size_t> stack, std::vector<std::string>& pointerVector)
 {
@@ -178,20 +98,11 @@ void IterateTable(Table table, Lexer& lexer, size_t ruleIndex, Token currentToke
 void AnalyzeTable(Table table, Lexer& lexer)
 {
 	std::cout << "Input sequence: ";
-	//for (auto index : inputSequence)
-	//{
-	//	std::cout << index + " ";
-	//}
 
 	std::cout << "\n";
 	std::stack<size_t> stack;
 	std::vector<std::string> pointerVector;
 	
 	IterateTable(table, lexer, 0, lexer.GetNextToken(), stack, pointerVector);
-	//std::cout << "Pointer sequence: ";
-	//for (auto index : pointerVector)
-	//{
-	//	std::cout << index + " ";
-	//}
 }
 
